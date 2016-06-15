1d33f18354a9b484568070dfc456d2243cd81d99
jasonbashov <jbashov@gmail.com>
2016-06-15 18:38:33 +0300

    Added class library and main entry point

---
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Game/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamJaxConsoleGame.Game
+{
+    /// <summary>
+    /// Class for the main entry point of the program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point of the console application.
+        /// </summary>
+        public static void Main()
+        {
+            var gameEngine = new Engine();
+            gameEngine.Run();
+        }
+    }
+}
