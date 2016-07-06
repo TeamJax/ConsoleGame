9ca8671b7c7c1c79e1e04660117909c2e5eee76e
Vasil Penev <ooze89yahoo.com>
2016-07-06 19:56:33 +0300

    Hero Class changes

--- TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Entities/HeroClasses/Monk.cs
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Entities/HeroClasses/Monk.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TeamJaxConsoleGame.Lib.Entities.HeroClasses
+﻿namespace TeamJaxConsoleGame.Lib.Entities.HeroClasses
 {
-    public class Monk
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TeamJaxConsoleGame.Lib.Enumerations;
+
+    public class Monk : Hero
     {
+        public Monk(string name, RaceType race) 
+            : base(name, race)
+        {
+        }
     }
 }
