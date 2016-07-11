fb72c2ef674cc96f20136590abb4c7a1c9be803a
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-07-11 22:05:37 +0300

    Added validator calss and error msgs class

---
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Common/Validator.cs
@@ -0,0 +1,31 @@
+﻿namespace TeamJaxConsoleGame.Lib.Common
+{
+    using System;
+
+    public static class Validator
+    {
+        public static void CheckIfNull(object value, string message)
+        {
+            if (value == null)
+            {
+                throw new NullReferenceException(message);
+            }
+        }
+
+        public static void CheckIfStringIsNullOrEmpty(string text, string message)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new NullReferenceException(message);
+            }
+        }
+
+        public static void ValidateIntRange(int value, int min, int max, string message)
+        {
+            if (value < min || value > max)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+    }
+}
---
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Constants/ValidatorConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TeamJaxConsoleGame.Lib.Constants
+{
+    internal class ValidatorConstants
+    {
+        internal const string ValueCannotBeNull = "{0} cannot be null or empty!";
+
+        internal const string StringMustBeBetweenMinAndMax = "{0} must be between {1} and {2} characters long!";
+    }
+}
