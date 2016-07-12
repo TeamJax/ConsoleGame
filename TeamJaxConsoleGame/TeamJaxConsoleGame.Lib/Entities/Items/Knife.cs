17359328b3db13eded0477ae059085a46e74c65e
Boriow <radev.boris@yahoo.com>
2016-07-12 14:46:28 +0300

    Refactoring Items, adding IAttackItem, IDefenseItem, IHealItem

--- TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Entities/Items/Knife.cs
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Entities/Items/Knife.cs
@@ -1,15 +1,28 @@
-﻿using TeamJaxConsoleGame.Lib.Enumerations;
+﻿using System;
+using TeamJaxConsoleGame.Lib.Enumerations;
+using TeamJaxConsoleGame.Lib.Interfaces;
 
 
 namespace TeamJaxConsoleGame.Lib.Entities.Items
 {
-    public class Knife : Item
+    public class Knife : Item, IAttackItem
     {
-        public const int knifeDamage = 20;
+        private const int knifeDamage = 20;
 
         public Knife(string name, int price)
             : base(name, price, ItemType.Knife)
         {
+            this.Damage = knifeDamage;
+        }
+
+        public int Damage
+        {
+            get; set;
+        }
+
+        public override string GiveDescription()
+        {
+            return string.Format("{0} and does {1} damage", base.GiveDescription(), this.Damage);
         }
 
     }
--- TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Entities/Items/Shield.cs
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Entities/Items/Shield.cs
@@ -1,18 +1,37 @@
-﻿using TeamJaxConsoleGame.Lib.Enumerations;
+﻿using System;
+using TeamJaxConsoleGame.Lib.Enumerations;
+using TeamJaxConsoleGame.Lib.Interfaces;
 
 
 namespace TeamJaxConsoleGame.Lib.Entities.Items
 {
-    class Shield : Item
+    class Shield : Item, IAttackItem, IDefenseItem
     {
-        public const int shieldDamage = 5;
-        public const int shieldProtection = 30;
+        private const int shieldDamage = 5;
+        private const int shieldProtection = 30;
 
         public Shield(string name, int price)
             : base(name, price, ItemType.Shield)
         {
+            this.Damage = shieldDamage;
+            this.Defense = shieldProtection;
         }
 
+        public int Damage
+        {
+            get; set;
+        }
+
+        public int Defense
+        {
+            get; set;
+        }
+
+        public override string GiveDescription()
+        {
+            return string.Format("{0}, has {1} defense power and does {2} damage", 
+                base.GiveDescription(),this.Defense, this.Damage);
+        }
 
     }
 }
