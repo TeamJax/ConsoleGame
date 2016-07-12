a5d8660e11f78f40e32c0ea28a34a38330ddef71
Vasil Penev <ooze89yahoo.com>
2016-07-12 12:16:46 +0300

    Priest , Rogue skill and abilities list

--- TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/PriestFactory.cs
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/PriestFactory.cs
@@ -4,9 +4,10 @@
     using Constants;
     using Entities;
     using Entities.HeroClasses;
-    using Enumerations;    
+    using Enumerations;
     using Interfaces;
-
+    using Entities.Abilities;
+    using Entities.Skills;
     public class PriestFactory : HeroFactory
     {
         public override Hero CreateHero(string name, RaceType race)
@@ -23,12 +24,23 @@
 
         public override IList<IAbility> DefinedHeroAbilities()
         {
-            throw new System.NotImplementedException();
+            return new List<IAbility>
+            {
+                new DamageAbility("Mind Blast", 20, 25, "Blasts the target for Shadow damage!"),
+                new DamageAbility("Shadow Word: Pain", 10, 15, "A word of darkness that causes Shadow damage and an additional  Shadow damage over 18 sec."),
+                new DamageAbility("Mind Flay",10, 20, "Assault the target's mind with Shadow energy,Shadow damage over 3 sec."),
+                new HealAbility("Penance", 5, 25, "Launches a volley of holy light at the target, causing Holy damage to an enemy and healing to an ally over 2 sec.")
+            };
         }
 
         public override IList<ISkill> DefinedHeroSkills()
         {
-            throw new System.NotImplementedException();
+            return new List<ISkill>
+            {
+                new PassiveSkill("Mastery: Shield Discipline", 25, "Increases the potency of all your damage absorption spells, and all of your healing."),
+                new ActiveSkill("Power Word: Fortitude", 5, 30, "Infuses the target with vitality, increasing Stamina by 10% for 1 hour.")
+
+            };
         }
     }
 }
--- TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/RogueFactory.cs
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/RogueFactory.cs
@@ -6,7 +6,8 @@
     using Enumerations;
     using System.Collections.Generic;
     using Interfaces;
-
+    using Entities.Abilities;
+    using Entities.Skills;
     public class RogueFactory : HeroFactory
     {
         public override Hero CreateHero(string name, RaceType race)
@@ -23,12 +24,22 @@
 
         public override IList<IAbility> DefinedHeroAbilities()
         {
-            throw new System.NotImplementedException();
+            return new List<IAbility>
+            {
+                new DamageAbility("Mutilate",5, 10, "Attack with both weapons, dealing damage with the main-hand and damage with the off-hand."),
+                new DamageAbility("Backstab", 10, 10, "Backstab the target, causing Physical damage.  Must not be in front of the target"),
+                new DamageAbility("Killing Spree", 20, 25, "Destroying evertyhing spreee physical dmg everywhere!"),
+                new HealAbility("Recuperate", 10, 15, "Finishing move that restores 3% of maximum health every 3 sec")
+            };
         }
 
         public override IList<ISkill> DefinedHeroSkills()
         {
-            throw new System.NotImplementedException();
+            return new List<ISkill>
+            {
+                new PassiveSkill("Swiftblade's Cunning", 20, "Increases stats of all party and raid members."),
+                new ActiveSkill("Stealth", 25, 30, "Conceals you in the shadows until cancelled, allowing you to stalk enemies without being seen.")
+            };
         }
     }
 }
