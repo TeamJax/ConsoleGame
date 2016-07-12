6c80ae5fa7f5336a10f049b15c31cacc5c3186f7
Vasil Penev <ooze89yahoo.com>
2016-07-12 12:02:10 +0300

    Monk and Paladin Lists

--- TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/MonkFactory.cs
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/MonkFactory.cs
@@ -6,7 +6,8 @@
     using Enumerations;
     using System.Collections.Generic;
     using Interfaces;
-
+    using Entities.Abilities;
+    using Entities.Skills;
     public class MonkFactory : HeroFactory
     {
         public override Hero CreateHero(string name, RaceType race)
@@ -23,12 +24,22 @@
 
         public override IList<IAbility> DefinedHeroAbilities()
         {
-            throw new System.NotImplementedException();
+            return new List<IAbility>
+            {
+                new DamageAbility("Fists of Fury", 20, 25, "Pummel all targets in front of you, stunning them and dealing damage!"),
+                new DamageAbility("Tiger Palm", 10, 20, "Attack with the palm of your hand, dealing damange!"),
+                new DamageAbility("Blackout Kick", 10, 15, "Kick with a blast of Chi energy, dealing damange"),
+                new HealAbility("Soothing Mist", 5, 10, "Heals the target for  over 8 sec!")
+            };
         }
 
         public override IList<ISkill> DefinedHeroSkills()
         {
-            throw new System.NotImplementedException();
+            return new List<ISkill>
+            {
+                new PassiveSkill("Stance of the Fierce Tiger", 30, "Grants you and your allies within 10 yards 10% increased movement speed!"),
+                new ActiveSkill("Fortifying Brew", 20, 25, "Reduces damage taken and increases maximum health by 20% for 15 seconds!")
+            };
         }
     }
 }
--- TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/PaladinFactory.cs
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/PaladinFactory.cs
@@ -4,9 +4,10 @@
     using Constants;
     using Entities;
     using Entities.HeroClasses;
-    using Enumerations;    
+    using Enumerations;
     using Interfaces;
-
+    using Entities.Abilities;
+    using Entities.Skills;
     public class PaladinFactory : HeroFactory
     {
         public override Hero CreateHero(string name, RaceType race)
@@ -23,12 +24,22 @@
 
         public override IList<IAbility> DefinedHeroAbilities()
         {
-            throw new System.NotImplementedException();
+            return new List<IAbility>
+            {
+                new DamageAbility("Crusader Strinke", 10, 20, "An instant strike that causes Physical damage."),
+                new DamageAbility("Judment Strike", 5, 15, " Causes Holy damage"),
+                new DamageAbility("Hammer of Wraith", 15, 25, "Hurls a magical hammer that strikes an enemy for Holy damage"),
+                new HealAbility("Holy Light", 10, 20, "Heals a friendly target!")
+            };
         }
 
         public override IList<ISkill> DefinedHeroSkills()
         {
-            throw new System.NotImplementedException();
+            return new List<ISkill>
+            {
+                new PassiveSkill("Righteous Fury", 20, "Increases your threat generation while active, making you a more effective tank."),
+                new ActiveSkill("Blessing of Might", 15, 20, " Places a Blessing on the friendly target, increasing stats by 50.")
+            };
         }
     }
 }
