d25616f331592c38bcc13e58b91bfa37787010ff
Vasil Penev <ooze89yahoo.com>
2016-07-12 11:43:36 +0300

    Mage ability lists

--- TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/MageFactory.cs
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Factory/HeroFactory/ClassesFactory/MageFactory.cs
@@ -6,7 +6,8 @@
     using Enumerations;
     using System.Collections.Generic;
     using Interfaces;
-
+    using Entities.Abilities;
+    using Entities.Skills;
     public class MageFactory : HeroFactory
     {
         public override Hero CreateHero(string name, RaceType race)
@@ -23,12 +24,23 @@
 
         public override IList<IAbility> DefinedHeroAbilities()
         {
-            throw new System.NotImplementedException();
+            return new List<IAbility>
+            {
+                new DamageAbility("Fire Ball", 10, 25, "Hurls a Fire Ball that cases fire damage to the target!"),
+                new DamageAbility("PyroBlast", 15, 30 ,"Hurls Immense fiery boulder that crushes the enemy and burnts it alive!"),
+                new DamageAbility("Arcane Blast", 10, 20,"Blast that launches strong arcane damage to the target!"),
+                new DamageAbility("Frost Bolt", 15, 25, "Launches a frost bolt that freezes and damages the target!")
+
+            };
         }
 
         public override IList<ISkill> DefinedHeroSkills()
         {
-            throw new System.NotImplementedException();
+            return new List<ISkill>
+            {
+                new PassiveSkill("Arcane Brilliance",50,"Infuses the target with arcane brilliance inscreasting their power!"),
+                new ActiveSkill("Summon Water Elementa", 15, 40,"Summons a water elemental to fight for the caster!")
+            };
         }
     }
 }
